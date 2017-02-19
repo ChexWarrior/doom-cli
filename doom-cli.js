72998a02bf8fd8fba333832e70e01ff673a0577e
Andrew Lehman <aplehm@gmail.com>
2017-02-19 09:02:59 -0500

    Test nodejs http requests

--- doom-cli.js
+++ doom-cli.js
@@ -1,6 +1,27 @@
 const http = require('http');
-const argv = require('minimist');
+//const argv = require('minimist');
 
-const apiEndpoint = "http://www.doomworld.com/idgames/api/api.php";
+const apiHost = 'www.doomworld.com';
+const apiPath = '/idgames/api/api.php';
+const apiAction = '?action=ping&out=json';
 
+const options = {
+  host: apiHost,
+  path: apiPath + apiAction
+};
 
+var request = http.request(options, (response) => {
+  var responseData = '';
+  
+  response.on('data', (data) => {
+    responseData += data;
+  });
+
+  response.on('end', () => {
+    console.log(responseData);
+  });
+
+  response.on('error', (error) => {
+    console.log(`Error: ${error.message}`);
+  });
+}).end();
