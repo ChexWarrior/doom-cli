7979d5ac1c497224d20cd6bce7fbea25e7f38f62
Andrew Lehman <aplehm@gmail.com>
2017-02-19 15:38:55 -0500

    Add handling of args

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 const http = require('http');
-//const argv = require('minimist');
+const argv = require('minimist')(process.argv.slice(2));
 
 const apiHost = 'www.doomworld.com';
 const apiPath = '/idgames/api/api.php';
-const apiAction = '?action=ping&out=json';
 
-const options = {
+function handleArgs(args) {
+  let searchText = args._;
+
+  // join all positional args together to form search string
+  if(searchText) {
+    return searchText.join(' ');
+  } else {
+    return false;
+  }
+}
+
+const searchText = handleArgs(argv);
+const apiAction = `?action=search&out=json&type=title&query=${searchText}`;
+const requestOptions = {
   host: apiHost,
   path: apiPath + apiAction
 };
 
-var request = http.request(options, (response) => {
+let request = http.request(requestOptions, (response) => {
   var responseData = '';
   
   response.on('data', (data) => {
@@ -25,3 +37,4 @@ var request = http.request(options, (response) => {
     console.log(`Error: ${error.message}`);
   });
 }).end();
+
