ab247be99c4d10a90d03e3ec6bbae32c39eb208d
Andrew Lehman <aplehm@gmail.com>
2017-02-20 07:54:33 -0500

    Ues promises when making request

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const http = require('http');
 const argv = require('minimist')(process.argv.slice(2));
+const Promise = require('promise');
 const apiHost = 'www.doomworld.com';
 const apiPath = '/idgames/api/api.php';
+
+function formatResults(results) {
+  console.dir(results);
+}
+
+function handleError(error) {
+  console.log(`Error: ${error}`);
+}
 
 function handleArgs(args) {
   let finalArgs = {};
@@ -20,29 +29,30 @@ function handleArgs(args) {
 }
 
 function makeRequest(action, host, path) {
-  const requestOptions = {
-    host: host,
-    path: path + action
-  };
+  return new Promise(function (resolve, reject) {
+    const requestOptions = {
+      host: host,
+      path: path + action
+    };
 
-  let request = http.request(requestOptions, (response) => {
-    var result = '';
-    
-    response.on('data', (data) => {
-      result += data;
-    });
+    let request = http.request(requestOptions, (response) => {
+      var result = '';
+      
+      response.on('data', (data) => {
+        result += data;
+      });
 
-    response.on('end', () => {
-      console.log(result);
-    });
+      response.on('end', () => {
+        resolve(result);
+      });
 
-    response.on('error', (error) => {
-      console.log(`Error: ${error.message}`);
-    });
-  }).end();
+      response.on('error', (error) => {
+        reject(error.message);
+      });
+    }).end();
+  });
 }
 
 const searchArgs = handleArgs(argv);
 const apiAction = `?action=search&out=json&type=${searchArgs.type}&query=${searchArgs.query}`;
-
-makeRequest(apiAction, apiHost, apiPath);
+makeRequest(apiAction, apiHost, apiPath).then(formatResults, handleError);
